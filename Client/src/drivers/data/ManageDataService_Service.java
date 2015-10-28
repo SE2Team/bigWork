25d9a5810220affe98bbd0ef80d6cea0e547dde0
moeyui <894immyk@gmail.com>
2015-10-28 19:40:50 +0800

    修改gitignore

---
+++ Client/src/drivers/data/ManageDataService_Service.java
@@ -0,0 +1,45 @@
+package src.drivers.data;
+
+
+import src.dataservice.managedataservice.ManageDataService;
+import src.po.*;
+
+
+public class ManageDataService_Service {
+	DriverPO driverPO;
+	String driveNumber;
+	VehiclePO vehiclePO;
+	String vehicleNumber;
+
+		public void drive(ManageDataService manageDataService){
+			boolean isAddDriver=manageDataService.addDriver(driverPO);
+			if(isAddDriver==true){
+				System.out.println("Success!");
+			}
+			
+			boolean isDelDriver=manageDataService.deleteDriver(driverPO);
+			if(isDelDriver==true){
+				System.out.println("Success!");
+			}
+			
+			DriverPO isCheckDriver=manageDataService.checkDriver(driveNumber);
+			if(isCheckDriver==driverPO){
+				System.out.println("Success!");
+			}
+			
+			boolean isAddVehicle=manageDataService.addVehicle(vehiclePO);
+			if(isAddVehicle==true){
+				System.out.println("Success!");
+			}
+			
+			boolean isDelVehicle=manageDataService.deleteVehicle(vehiclePO);
+			if(isDelVehicle==true){
+				System.out.println("Success!");
+			}
+			
+			VehiclePO isCheckVehicle=manageDataService.checkVehicle(vehicleNumber);
+			if(isCheckVehicle==vehiclePO){
+				System.out.println("Success!");
+			}
+		}
+}
